ce266d956b96c070a961a4b4906faa39e89616b7
Mhamad mahdi Chahine <mmchahine0@gmail.com>
2025-07-03 19:03:53 +0300

    deployment 2

--- client/src/features/home/Home.component.tsx
+++ client/src/features/home/Home.component.tsx
@@ -269,42 +269,6 @@ export default function HomePageContent() {
     }
   };
 
-
-  const featuredLoading = () =>{
-// Show loading skeleton with animation
-if (isLoading) {
-  return (
-    <div className="min-h-screen flex flex-col animate-pulse">
-
-
-      {/* Featured Items Skeleton */}
-      <section className="py-16 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
-          <div className="h-8 bg-gray-300 rounded mb-10 mx-auto w-48"></div>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
-            {Array.from({ length: 4 }).map((_, index) => (
-              <div
-                key={index}
-                className="bg-white border border-gray-200 rounded-xl overflow-hidden shadow-md"
-              >
-                <div className="p-4 flex flex-col h-full">
-                  <div className="flex-1 bg-gray-300 rounded-lg mb-4 h-60"></div>
-                  <div className="space-y-2">
-                    <div className="h-6 bg-gray-300 rounded"></div>
-                    <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-                    <div className="h-4 bg-gray-300 rounded w-1/2"></div>
-                    <div className="h-10 bg-gray-300 rounded mt-4"></div>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-}
-  }
   
 
   return (
